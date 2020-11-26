1dec4195de24a6e5de3942caefa7adf730aceeaa
Guanyu Wu <lambda@funcptr.org>
2020-11-26 13:42:03 -0500

    Added geyser configs and maybe future floodgate?

--- proxy/plugins/DownloadPlugins.sh
+++ proxy/plugins/DownloadPlugins.sh
@@ -7,3 +7,6 @@ rm Advanced-Portals-*.jar
 wget "https://github.com/sekwah41/Advanced-Portals/releases/download/$ADVANCED_PORTALS_VERSION/Advanced-Portals-$ADVANCED_PORTALS_VERSION-snapshot.jar"
 
 wget "https://ci.nukkitx.com/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/bungeecord/target/Geyser-BungeeCord.jar"
+
+# We can eventually do something like this... However, we're not going to do it now.
+#wget "https://ci.nukkitx.com/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/bungee/target/floodgate-bungee.jar"
