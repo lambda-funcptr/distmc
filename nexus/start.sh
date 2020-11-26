a8d595b355f2dba8b9f287c6eecdf21bfcaa8574
Guanyu Wu <lambda@funcptr.org>
2020-11-25 21:19:57 -0500

    Some basic changes to distmc.

--- nexus/start.sh
+++ nexus/start.sh
@@ -4,4 +4,5 @@ cd $(dirname $0)
 
 echo "eula=true" > eula.txt
 
-java -server -XX:+UseG1GC -XX:MaxGCPauseMillis=500 -XX:+UseStringDeduplication -Xmx1G -Xms1G -XX:UseSSE=3 -jar  server.jar nogui
+tmux new -s console -d "/usr/lib/jvm/java-11-openjdk/bin/java -server -XX:+UseG1GC -XX:MaxGCPauseMillis=500 -XX:+UseStringDeduplication -Xmx1G -Xms1G -XX:UseSSE=3 -jar  server.jar nogui"
+tmux wait-for console
--- proxy/start.sh
+++ proxy/start.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)
 
 cat config.yml.template > config.yml
 
-java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar proxy.jar
+/usr/lib/jvm/java-11-openjdk/bin/java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar proxy.jar
