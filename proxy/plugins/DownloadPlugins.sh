2254fc84c4412d7e59b2506fb26e587e381bb73b
Guanyu Wu <lambda@funcptr.org>
2020-11-26 13:27:06 -0500

    Added GeyserMC.

--- proxy/plugins/DownloadPlugins.sh
+++ proxy/plugins/DownloadPlugins.sh
@@ -5,3 +5,5 @@ cd $(dirname $0)
 ADVANCED_PORTALS_VERSION=0.5.10
 rm Advanced-Portals-*.jar
 wget "https://github.com/sekwah41/Advanced-Portals/releases/download/$ADVANCED_PORTALS_VERSION/Advanced-Portals-$ADVANCED_PORTALS_VERSION-snapshot.jar"
+
+wget "https://ci.nukkitx.com/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/bungeecord/target/Geyser-BungeeCord.jar"
