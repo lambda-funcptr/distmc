d42924a9fdb0da82480761053afd68859ba4bef7
Guanyu Wu <lambda@funcptr.org>
2021-01-02 15:16:47 -0500

    Typos everywhere. Also updated AdvancedPortals version.

--- nexus/plugins/DownloadPlugins.sh
+++ nexus/plugins/DownloadPlugins.sh
@@ -2,6 +2,6 @@
 
 cd $(dirname $0)
 
-ADVANCED_PORTALS_VERSION=0.5.10
+ADVANCED_PORTALS_VERSION=0.5.12
 rm Advanced-Portals-*.jar
 wget "https://github.com/sekwah41/Advanced-Portals/releases/download/$ADVANCED_PORTALS_VERSION/Advanced-Portals-$ADVANCED_PORTALS_VERSION-snapshot.jar"
