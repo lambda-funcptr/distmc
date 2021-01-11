5579227b79ee93631e0be2e2488496dacfc46b7e
Guanyu Wu <lambda@funcptr.org>
2021-01-11 13:16:22 -0500

    Removed tmux in docker. Let's not do this again.

--- nexus/start.sh
+++ nexus/start.sh
@@ -4,5 +4,4 @@ cd $(dirname $0)
 
 echo "eula=true" > eula.txt
 
-tmux new -s console -d "/usr/lib/jvm/java-11-openjdk/bin/java -server -XX:+UseG1GC -XX:MaxGCPauseMillis=500 -XX:+UseStringDeduplication -Xmx1G -Xms1G -XX:UseSSE=3 -jar  server.jar nogui"
-tmux wait-for console
+/usr/lib/jvm/java-11-openjdk/bin/java -server -XX:+UseG1GC -XX:MaxGCPauseMillis=500 -XX:+UseStringDeduplication -Xmx1G -Xms1G -XX:UseSSE=3 -jar  server.jar nogui
